7f76f87606a4b477ad639391ead595cc9f7d6d17
Chetan Hire <chire@proteowise.com>
2025-03-21 21:02:15 -0400

    added 3 additional columns

--- Beta Run Data2.R
+++ Beta Run Data2.R
@@ -6,7 +6,9 @@ fi <- list.files(pattern = "ReagentLog.json",
   recursive = TRUE, full.names = TRUE
 )
 
-summary_data <- data.frame(carrierID = character(), data_type = character(),
+summary_data <- data.frame(carrierID = character(), ctg_pkg_info = character(),
+  ctg_prep_notes = character(), data_type = character(),
+  cycle_end_date = integer(),
   index = integer(), run_num = integer(), label = character(),
   experiment_name = character(), script_name = character(),
   avg_final_wash_thru_dp = numeric(), rig_Name = character(),
@@ -55,8 +57,7 @@ for (i in 2: length(fi)) {
       summ_data$rig_Name[a] <- json_data$`Run Details`[[j]]$`Rig Name`
       summ_data$run_num[a] <- json_data$`Run Details`[[j]]$`Run Num`
       summ_data$run_name[a] <- json_data$`Run Details`[[j]]$`Run Name`
-      summ_data$additional_notes[a] <-
-        json_data$`Run Details`[[j]]$`Run Completion Details`$`Additional Notes`
+      summ_data$additional_notes[a] <- as.character(json_data$`Run Details`[[j]]$`Run Completion Details`$`Additional Notes`)
     }
 
     if (length(json_data$`Cycle Data`) > 0) {
@@ -76,11 +77,19 @@ for (i in 2: length(fi)) {
         summ_data$avg_final_wash_thru_dp[b] <-
           json_data$`Cycle Data`[[k]]$cycle_metrics[[1]]
 
+        if ("cycle_end_date" %in% names(json_data$`Cycle Data`[[k]])) {
+          summ_data$cycle_end_date[b] <-
+            as.integer(json_data$`Cycle Data`[[k]]$cycle_end_date)
+        }
+
         if (length(json_data$`Run Details`) >=
               json_data$`Cycle Data`[[k]]$run_num) {
-          summ_data$rig_Name[b] <- json_data$`Run Details`[[json_data$`Cycle Data`[[k]]$run_num]]$`Rig Name`
-          summ_data$run_name[b] <- json_data$`Run Details`[[json_data$`Cycle Data`[[k]]$run_num]]$`Run Name`
-          summ_data$additional_notes[b] <- json_data$`Run Details`[[json_data$`Cycle Data`[[k]]$run_num]]$`Run Completion Details`$`Additional Notes`
+          parent_run_num <- json_data$`Cycle Data`[[k]]$run_num
+          summ_data$rig_Name[b] <-
+            json_data$`Run Details`[[parent_run_num]]$`Rig Name`
+          summ_data$run_name[b] <-
+            json_data$`Run Details`[[parent_run_num]]$`Run Name`
+          summ_data$additional_notes[b] <- as.character(json_data$`Run Details`[[parent_run_num]]$`Run Completion Details`$`Additional Notes`)
         } else {
           summ_data$rig_Name[b] <- "DNF"
           summ_data$run_name[b] <- "DNF"
@@ -99,14 +108,26 @@ for (i in 2: length(fi)) {
         summ_data$index[b] <- json_data$`Non-Detect Cycle Data`[[i]]$index
         summ_data$run_num[b] <- json_data$`Non-Detect Cycle Data`[[i]]$run_num
         summ_data$label[b] <- json_data$`Non-Detect Cycle Data`[[i]]$label
-        summ_data$experiment_name[b] <- json_data$`Non-Detect Cycle Data`[[i]]$experiment_name
-        summ_data$script_name[b] <- json_data$`Non-Detect Cycle Data`[[i]]$script_name
-        summ_data$avg_final_wash_thru_dp[b] <- json_data$`Non-Detect Cycle Data`[[i]]$cycle_metrics[[1]]
+        summ_data$experiment_name[b] <-
+          json_data$`Non-Detect Cycle Data`[[i]]$experiment_name
+        summ_data$script_name[b] <-
+          json_data$`Non-Detect Cycle Data`[[i]]$script_name
+        summ_data$avg_final_wash_thru_dp[b] <-
+          json_data$`Non-Detect Cycle Data`[[i]]$cycle_metrics[[1]]
 
-        if (length(json_data$`Run Details`) >= json_data$`Non-Detect Cycle Data`[[i]]$run_num) {
-          summ_data$rig_Name[b] <- json_data$`Run Details`[[json_data$`Non-Detect Cycle Data`[[i]]$run_num]]$`Rig Name`
-          summ_data$run_name[b] <- json_data$`Run Details`[[json_data$`Non-Detect Cycle Data`[[i]]$run_num]]$`Run Name`
-          summ_data$additional_notes[b] <- json_data$`Run Details`[[json_data$`Non-Detect Cycle Data`[[i]]$run_num]]$`Run Completion Details`$`Additional Notes`
+        if ("cycle_end_date" %in% names(json_data$`Non-Detect Cycle Data`[[i]])) {
+          summ_data$cycle_end_date <
+            as.integer(json_data$`Non-Detect Cycle Data`[[i]]$cycle_end_date)
+        }
+
+        if (length(json_data$`Run Details`) >=
+              json_data$`Non-Detect Cycle Data`[[i]]$run_num) {
+          parent_run_num <- json_data$`Non-Detect Cycle Data`[[i]]$run_num
+          summ_data$rig_Name[b] <-
+            json_data$`Run Details`[[parent_run_num]]$`Rig Name`
+          summ_data$run_name[b] <-
+            json_data$`Run Details`[[parent_run_num]]$`Run Name`
+          summ_data$additional_notes[b] <- as.character(json_data$`Run Details`[[parent_run_num]]$`Run Completion Details`$`Additional Notes`)
         } else {
           summ_data$rig_Name[b] <- "DNF"
           summ_data$run_name[b] <- "DNF"
@@ -116,12 +137,14 @@ for (i in 2: length(fi)) {
     }
     # for (j in 1: length(json_data$`Lane Content`)) { # nolint
     for (j in seq_len(length(json_data$`Lane Content`))) {
-      summ_data[, 11 + 2 * j] <- json_data$`Lane Content`[[j]]$sample
-      summ_data[, 12 + 2 * j] <- json_data$`Lane Content`[[j]]$concentration
+      summ_data[, 14 + 2 * j] <- json_data$`Lane Content`[[j]]$sample
+      summ_data[, 15 + 2 * j] <- json_data$`Lane Content`[[j]]$concentration
     }
 
     summ_data$no_of_lanes <- length(json_data$`Lane Content`)
     summ_data$carrierID <- carr_id
+    summ_data$ctg_pkg_info <- json_data$`Cartridge ID`
+    summ_data$ctg_prep_notes <- json_data$`Carrier Rev`
     summary_data <- bind_rows(summary_data, summ_data)
   }
 }
